b5307bc1bfee0dfcc12d5d927cf626fe7b80d74e
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2022-03-29 12:19:06 -0400

    added download page

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,7 @@
 let header_code = `
 <div id="nav_bar">
     <a id="home_nav_button" class="nav_entry" href="index.html" style="float:left;padding:2px;"><img class="nav_bar_logo" src="res/logo.svg"></a>
-    <a id="download_nav_button" class="nav_entry">Download</a>
-    <a id="progress_nav_button" class="nav_entry">Progress</a>
+    <a id="download_nav_button" class="nav_entry" href="download.html">Download</a>
     <a id="contact_nav_button" class="nav_entry">Contact Us</a>
     <a id="about_nav_button" class="nav_entry" href="about.html">About</a>
 </div>
